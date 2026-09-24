1741950b70503283a480a2f26d0817baf5cb623e
levidepsi7 <levidepsi@gmail.com>
2026-09-24 14:47:37 +0800

    websocket & tanstack

--- src/realtime/realtime-connection.tsx
+++ src/realtime/realtime-connection.tsx
@@ -5,6 +5,24 @@ import { socket } from "./socket";
 
 export default function RealtimeConnection() {
   useEffect(() => {
+    const handleConnect = () => {
+      console.log("Realtime connected:", socket.id);
+    };
+
+    const handleDisconnect = (reason: string) => {
+      console.log("Realtime disconnected:", reason);
+    };
+
+    socket.on("connect", handleConnect);
+    socket.on("disconnect", handleDisconnect);
+
+    return () => {
+      socket.off("connect", handleConnect);
+      socket.off("disconnect", handleDisconnect);
+    };
+  }, []);
+
+  useEffect(() => {
     const handleCustomerCreated = (data: unknown) => {
       console.log("Realtime customer.created:", data);
     };
@@ -27,18 +45,6 @@ export default function RealtimeConnection() {
       socket.off("customer.deleted", handleCustomerDeleted);
     };
   }, []);
-
-  // useEffect(() => {
-  //   const handleCustomerCreated = (data: unknown) => {
-  //     console.log("Realtime customer.created:", data);
-  //   };
-
-  //   socket.on("customer.created", handleCustomerCreated);
-
-  //   return () => {
-  //     socket.off("customer.created", handleCustomerCreated);
-  //   };
-  // }, []);
 
   return null;
 }

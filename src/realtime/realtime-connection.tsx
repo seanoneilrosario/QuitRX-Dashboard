f0c42db1282e4917f718608435898bf581529519
levidepsi7 <levidepsi@gmail.com>
2026-09-24 14:34:55 +0800

    websocket & tanstack

--- src/realtime/realtime-connection.tsx
+++ src/realtime/realtime-connection.tsx
@@ -8,23 +8,29 @@ export default function RealtimeConnection() {
     const handleConnect = () => {
       console.log("Realtime connected:", socket.id);
     };
+
     const handleDisconnect = (reason: string) => {
       console.log("Realtime disconnected:", reason);
-    };
-    const handleConnectError = (error: Error) => {
-      console.error("Realtime connection error:", error.message);
     };
 
     socket.on("connect", handleConnect);
     socket.on("disconnect", handleDisconnect);
-    socket.on("connect_error", handleConnectError);
-    socket.connect();
 
     return () => {
-      socket.disconnect();
       socket.off("connect", handleConnect);
       socket.off("disconnect", handleDisconnect);
-      socket.off("connect_error", handleConnectError);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleCustomerCreated = (data: unknown) => {
+      console.log("Realtime customer.created:", data);
+    };
+
+    socket.on("customer.created", handleCustomerCreated);
+
+    return () => {
+      socket.off("customer.created", handleCustomerCreated);
     };
   }, []);
 

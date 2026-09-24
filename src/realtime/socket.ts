2e59675508216d2d251efacb29f1d021a24f4f0c
levidepsi7 <levidepsi@gmail.com>
2026-09-24 14:30:28 +0800

    websocket & tanstack

--- src/realtime/socket.ts
+++ src/realtime/socket.ts
@@ -1,10 +1,16 @@
 "use client";
 
+// import { io } from "socket.io-client";
+
+// export const socket = io("https://retail-api.quithero.com.au/realtime", {
+//   transports: ["websocket"],
+//   withCredentials: true,
+//   // Connect after mounting so server rendering never opens a connection.
+//   autoConnect: false,
+// });
+
 import { io } from "socket.io-client";
 
 export const socket = io("https://retail-api.quithero.com.au/realtime", {
-  transports: ["websocket"],
   withCredentials: true,
-  // Connect after mounting so server rendering never opens a connection.
-  autoConnect: false,
 });
